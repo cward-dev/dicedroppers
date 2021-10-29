469885137bf8f608f106fc6150197a6ddab374ad
Chris Ward <cshonward@gmail.com>
2021-10-28 22:24:08 -0500

    navbar updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef, useCallback, setState, classNames, state, toggleDropdown, isOpen, responsive } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,38 +8,60 @@ import {
 } from "react-router-dom";
 import "./Nav.css";
 
+function useEventListener(eventName, handler, element = window) {
+  const savedHandler = useRef();
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+  useEffect(
+    () => {
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+      const eventListener = (event) => savedHandler.current(event);
+      element.addEventListener(eventName, eventListener);
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element]
+  );
+}
+
 function Nav() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleLink = () => {
+    setIsActive(false);
+  };
+
+  const handleIcon = () => {
+    setIsActive(isActive ? false : true);
+  };
+
+  const handler = () => {
+    setIsActive(false);
+  };
+
+  useEventListener("mouseup", handler);
+
   const logoPath = "/images/logo.png";
 
   return (
-    <ul className="nav">
-      <li className="nav-logo-container nav-item-left">
-        <Link to="/" exact={true}>
-          <img className="nav-logo" src={logoPath} alt="Logo" />
-        </Link>
-      </li>
-      <li className="nav-item nav-item-left">
-        <Link to="/" exact={true}>Dice Droppers</Link>
-      </li>
-      <li className="nav-item nav-item-left">
-        <NavLink to="/" exact={true}>Home</NavLink>
-      </li>
-      <li className="nav-item nav-item-left">
-        <NavLink to="/blog">Blog</NavLink>
-      </li>
-      <li className="nav-item nav-item-left">
-        <NavLink to="/episodes">Episodes</NavLink>
-      </li>
-      <li className="nav-item nav-item-left">
-        <NavLink to="/campaign-notes">Campaign Notes</NavLink>
-      </li>
-      <li className="nav-item nav-item-left">
-        <NavLink to="/meet-the-crew">Meet The Crew</NavLink>
-      </li>
-      <li className="nav-item nav-item-right">
-        <NavLink to="/search">Search</NavLink>
-      </li>
-    </ul>
+    <div className={`nav${isActive ? " responsive" : ""}`} id="myNav">
+      <div className="nav-logo-container">
+        <Link to="/" className="sticky nav-logo-link" exact={true}><img className="nav-logo" src={logoPath} alt="Logo" /></Link>
+        <Link to="/" className="sticky nav-name" exact={true}>Dice Droppers</Link>
+      </div>
+      <NavLink to="/" className="nav-item home" exact={true}>Home</NavLink>
+      <NavLink to="/blog" className="nav-item">Blog</NavLink>
+      <NavLink to="/episodes" className="nav-item">Episodes</NavLink>
+      <NavLink to="/campaign-notes" className="nav-item">Campaign Notes</NavLink>
+      <NavLink to="/meet-the-crew" className="nav-item">Meet The Crew</NavLink>
+      <NavLink to="/search" className="nav-item">Search</NavLink>
+      <a href="javascript:void(0);" className="nav-item icon" onClick={handleIcon}>
+        <i className="fa fa-bars" />
+      </a>
+    </div>
   );
 }
 
--- src/pages/campaign-notes/campaign-notes.js
+++ src/pages/campaign-notes/campaign-notes.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import "../pages.css";
+import "./campaign-notes.css";
 
 function CampaignNotes() {
 
@@ -8,6 +9,7 @@ function CampaignNotes() {
       <div className="wrapper">
         <h1>Campaign Notes</h1>
         <p>Get up to speed.</p>
+        <img className="world-map" src="/images/world-map.jpg" />
       </div>
     </div>
   );
