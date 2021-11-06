2e40c3e7cae4c19712ffbe9df4d0b250aa4e466d
Chris Ward <cshonward@gmail.com>
2021-11-06 15:12:27 -0500

    removed console log

--- src/utils/data-api.js
+++ src/utils/data-api.js
@@ -7,6 +7,5 @@ export const karstSunborne = "/Karst_Sunborne.json"
 export const getData = async (folderName, fileName) => {
   const response = await fetch(`${baseUrl}/${folderName}/${fileName}.json`);
   var data = await response.json();
-  console.log(data);
   return data;
 };
